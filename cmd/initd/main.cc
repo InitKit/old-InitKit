798d438abbc508b74d4a3ac2e661f544be69c695
NetaScale Developer <developers@netascale.com>
2021-06-27 10:37:05 +0100

    Initial Commit

---
+++ cmd/initd/main.cc
@@ -0,0 +1,17 @@
+  
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 		PROPRIETARY NOTICE:
+ *
+ * The software contained on this media is unpublished proprietary information
+ * constituting or derived under licence from NetaScale Advanced Executive (tm),
+ * a product of NetaScale Systems, Ltd.
+ *
+ * 		Copyright Notice:
+ *
+ * Notice of copyright on this source code product does not indicate
+ * publication.
+ *
+ * 	(c) 2008-2021 NetaScale Systems, Ltd.
+ * 		All rights reserved.
+ */
---
+++ prototypes/prototype.h
@@ -0,0 +1,50 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 		PROPRIETARY NOTICE:
+ *
+ * The software contained on this media is unpublished proprietary information
+ * constituting or derived under licence from NetaScale Advanced Executive (tm),
+ * a product of NetaScale Systems, Ltd.
+ *
+ * 		Copyright Notice:
+ *
+ * Notice of copyright on this source code product does not indicate
+ * publication.
+ *
+ * 	(c) 2008-2021 NetaScale Systems, Ltd.
+ * 		All rights reserved.
+ */
+/**
+ * @file filename.h
+ * @brief A brief description of the file's purpose.
+ *
+ * A lengthier description of the general purpose of the file, any subtleties to
+ * which the programmer's attention ought to be drawn, etc.
+ */
+
+#ifndef FILENAME_H_
+#define FILENAME_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <includes.h>
+
+#define MACROS values
+
+struct tag {
+	type_t member;
+};
+
+typedef predefined_types new_types;
+
+type_t global_variables;
+
+void functions(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FILENAME_H_ */ d
